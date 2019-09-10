e4e9382e5915a11e766bdf8864fd79b076969461
Tony Lykke <tl@hudson-trading.com>
2019-09-10 14:06:08 -0400

    cleanup definition tests to generate more cases

--- tests/ttypes/test_definition.py
+++ tests/ttypes/test_definition.py
@@ -27,31 +27,22 @@ def test_str_args(arg_def):
     assert len(actual.fields) == len(extract_args(arg_def))
 
 
-@given(
-    valid_definition_str(),
-    st.dictionaries(
-        st.shared(non_empty_string(), key="field"),
-        st.shared(non_empty_string(), key="field"),
-        min_size=1,
-    ),
-)
-def test_with_kwargs(arg_def, kwargs):
+@given(valid_definition_str(), st.data())
+def test_with_kwargs(arg_def, data):
     args = extract_args(arg_def)
+    kwargs = {k: k for k in data.draw(st.sets(non_empty_string(), min_size=1))}
+
     assume(not any(a in kwargs for a in args))
+
     actual = Definition(arg_def, **kwargs)
     assert len(actual.fields) == len(extract_args(arg_def)) + len(kwargs)
 
-@given(
-    valid_definition_str(),
-    non_empty_string(),
-    st.dictionaries(
-        st.shared(non_empty_string(), key="field"),
-        st.shared(non_empty_string(), key="field"),
-        min_size=1,
-    ),
-)
-def test_all_field_positions(arg_def, arg_field, kwargs):
+
+@given(valid_definition_str(), non_empty_string(), st.data())
+def test_all_field_positions(arg_def, arg_field, data):
     args = extract_args(arg_def)
+    kwargs = {k: k for k in data.draw(st.sets(non_empty_string(), min_size=1))}
+
     assume(arg_field not in args)
     assume(arg_field not in kwargs)
     assume(not any(a in kwargs for a in args))
@@ -60,17 +51,16 @@ def test_all_field_positions(arg_def, arg_field, kwargs):
     assert len(actual.fields) == len(args) + len(kwargs) + 1
 
 
-@given(
-    valid_definition_str(),
-    st.shared(non_empty_string(), key="field"),
-    st.dictionaries(
-        st.shared(non_empty_string(), key="field"),
-        st.shared(non_empty_string(), key="field"),
-        min_size=1,
-    ),
-)
-def test_all_field_positions(arg_def, arg_field, kwargs):
+@given(valid_definition_str(), st.shared(non_empty_string(), key="field"), st.data())
+def test_kwarg_overwrites_arg_field(arg_def, arg_field, data):
     args = extract_args(arg_def)
+    kwargs = {
+        k: k
+        for k in data.draw(
+            st.sets(st.shared(non_empty_string(), key="field"), min_size=1)
+        )
+    }
+
     assume(arg_field not in args)
     assume(not any(a in kwargs for a in args))
 

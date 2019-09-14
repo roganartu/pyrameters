459fbbe4ae8ce2bf449ffb1df3c5a527f0156df2
Tony Lykke <tl@hudson-trading.com>
2019-09-14 15:35:28 -0400

    run pytester sub runs in subprocess instead of in current process

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,3 +18,12 @@ hypothesis.settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
 # Register the utils module as a helper so that pytest knows to monkey-patch assert
 # to give nice diffs on failure.
 pytest.register_assert_rewrite("utils")
+
+# Make sure that pytester testdir runs are done as subprocess instead of inprocess.
+# Something we (or maybe hypothesis?) does isn't properly cleared by the testdir
+# fixture cleanup, so we get erroneous failures when using inprocess.
+# This method for accessing config is deprecated, but I can't see another way without
+# requiring the user to remember a cmdline arg.
+# The alternative to this is always passing --runpytest subprocess
+def pytest_configure(config):
+    config.option.__dict__["runpytest"] = "subprocess"
